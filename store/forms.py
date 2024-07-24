0bea841d9c130076694fcc68c507bed708d41582
Joeson-Suomie <joesuome@gmail.com>
2024-07-25 01:06:25 +0530

    Initial Commit

---
+++ store/forms.py
@@ -0,0 +1,15 @@
+from django import forms
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth import get_user_model
+
+CustomUser = get_user_model()
+
+class CustomUserCreationForm(UserCreationForm):
+    class Meta(UserCreationForm.Meta):
+        model = CustomUser
+        fields = ('username', 'email')  # Add any additional fields here
+
+class CustomAuthenticationForm(AuthenticationForm):
+    class Meta:
+        model = CustomUser
+        fields = ('username', 'password')
